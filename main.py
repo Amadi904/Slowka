09c930d6cdf9bd05b3ae3a15ba8172aae36edb7b
Amadi904 <amadeusz123>
2024-03-05 23:09:44 +0100

    Polaczenie z sql

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
-#tkinter
-#SQLite
-
+import  sqlite3
 import tkinter
 
-slownik=\
-    {
-    "chicken":"kurczak",
-        "dog":"pies",
-    }
+conn=sqlite3.connect('words.db')
+c=conn.cursor()
 
-def Tlumacz():
-    #pobiera slowo z lebela ktory jest w gornej czesci programu
-    slowo_angielskie=entry.get()
-    #przypisuje do zmiennej tlumaczenie wartosc sprawdzona z wartosciami ze slownika zapisana aktualenie w etykiecie
-    tlumaczenie=slownik.get(slowo_angielskie)
-    #jesli slowo znajduje sie w slowniku zwrace tlumaczenie
-    if tlumaczenie:
-        slowo.config(text=tlumaczenie)
-    else:
-        slowo.config(text="Brak slowa "+slowo_angielskie.upper()+" w slowniku")
+c.execute(''' CREATE TABLE IF NOT EXISTS words (
+    id INTEGER PRIMARY KEY,
+    english_word TEXT NOT NULL,
+    translated_word TEXT NOT NULL
+)
+''')
+conn.commit()
+words_to_add=[('apple','jablko'),('pear','gruszka')]
+c.executemany('INSERT INTO words (english_word, translated_word) VALUES (?, ?)', words_to_add)
 
-def dodajznane():
-   wartosc=entry.get()
-   ZnaneSlowa.append(wartosc)
-   print("dodano do listy")
-   for element in ZnaneSlowa:
-       print(element)
-ZnaneSlowa=[]
-root=tkinter.Tk()
+conn.commit()
+c.execute('SELECT *FROM words')
+rows=c.fetchall()
+for row in rows:
+    print(row)
+conn.close()
+
+
+
+root = tkinter.Tk()
 root.geometry("400x400")
-slowo=tkinter.Label(root, text="")
-slowo.pack()
+root.title('Aplikacja do Nauki')
 
-entry=tkinter.Entry(root)
-entry.pack()
-znane=tkinter.Button(root,text="Znane",background="green",foreground="white",command=dodajznane)
-znane.place(x=280,y=200)
-#znane.pack()
-nieznane=tkinter.Button(root,text="Nie znane",bg="red",fg="white")
-nieznane.place(x=70,y=200)
-#nieznane.pack()
-tlumacz=tkinter.Button(root,text="tlumacz",command=Tlumacz)
-tlumacz.place(x=190,y=200)
-#tlumacz.pack()
 
 
 root.mainloop()
